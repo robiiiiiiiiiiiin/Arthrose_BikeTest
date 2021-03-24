fb6915d51195c8149dbc94bccc23933b310ddc42
Robin <robinbecherraz2@hotmail.fr>
2021-03-24 19:03:54 +0100

    Changed mysql for pgsql

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +65,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DATABASE_URL')['host'],
+            'port' => env('DATABASE_URL')['port'],
+            'database' => ltrim(env('DATABASE_URL')['path'], '/'),
+            'username' => env('DATABASE_URL')['user'],
+            'password' => env('DATABASE_URL')['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
